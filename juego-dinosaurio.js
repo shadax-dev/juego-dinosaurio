be41cceed5c8d83f072ff7a9666cc10b2ad3b880
shadax-dev <p.adrian.escudero@pm.me>
2023-03-31 00:35:33 +0200

    feat: dinosaur is an image

--- juego-dinosaurio.js
+++ juego-dinosaurio.js
@@ -1,17 +1,23 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-// Definir una variable para controlar la velocidad del dinosaurio
-const dinoSpeed = 5;
+const dinoImage = new Image();
+dinoImage.crossOrigin = 'anonymous'; // Establecer la propiedad crossOrigin
+dinoImage.src = 'dinosaurio.png';
+dinoImage.onload = function() {
+    // Comenzar el bucle del juego
+    gameLoop();
+}
 
 // Definir el objeto del dinosaurio
 const dino = {
     x: 50,
-    y: 125,
+    y: canvas.height - 50,
     width: 50,
     height: 50,
     speed: 5,
-    jumping: false
+    jumping: false,
+    img: dinoImage
 };
 
 // Definir el objeto del obstáculo
@@ -31,16 +37,39 @@ const ground = {
     height: 50
 };
 
+// Gravedad
+const gravity = 0.5;
+
 // Dibujar el dinosaurio
 function drawDino() {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(dino.x, dino.y, dino.width, dino.height);
+    ctx.drawImage(dino.img, dino.x, dino.y, dino.width, dino.height);
 }
 
 // Dibujar el obstáculo
 function drawObstacle() {
     ctx.fillStyle = 'red';
     ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+}
+
+function getDinoPixels() {
+    const pixels = [];
+
+    const imgData = ctx.getImageData(dino.x, dino.y, dino.width, dino.height);
+
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        const r = imgData.data[i];
+        const g = imgData.data[i + 1];
+        const b = imgData.data[i + 2];
+        const a = imgData.data[i + 3];
+
+        if (r === 0 && g === 128 && b === 0 && a === 255) {
+            pixels.push(1);
+        } else {
+            pixels.push(0);
+        }
+    }
+
+    return pixels;
 }
 
 // Generador de obstáculos
@@ -54,10 +83,23 @@ function updateObstacle() {
     }
 }
 
-//New loop created
+// Bucle de juego
 function gameLoop() {
     // Limpiar el lienzo
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Actualizar la velocidad y la posición vertical del dinosaurio si está saltando
+    if (dino.jumping) {
+        dino.speed += gravity;
+        dino.y += dino.speed;
+
+        // Si el dinosaurio ha llegado al suelo, detener el salto
+        if (dino.y + dino.height > ground.y) {
+            dino.y = ground.y - (dino.height * 2);
+            dino.jumping = false;
+            dino.speed = 0;
+        }
+    }
 
     // Dibujar el dinosaurio
     drawDino();
@@ -79,31 +121,32 @@ function gameLoop() {
 
 // Verificar si hay colisión
 function checkCollision() {
-    const dinoLeft = dino.x;
-    const dinoRight = dino.x + dino.width;
-    const dinoTop = dino.y;
-    const dinoBottom = dino.y + dino.height;
+    const dinoPixels = getDinoPixels();
 
     const obstacleLeft = obstacle.x;
     const obstacleRight = obstacle.x + obstacle.width;
     const obstacleTop = obstacle.y;
     const obstacleBottom = obstacle.y + obstacle.height;
 
-    if (
-        dinoLeft < obstacleRight &&
-        dinoRight > obstacleLeft &&
-        dinoTop < obstacleBottom &&
-        dinoBottom > obstacleTop
-    ) {
-        return true;
+    for (let i = 0; i < dinoPixels.length; i++) {
+        const row = Math.floor(i / dino.width);
+        const col = i % dino.width;
+
+        if (dinoPixels[i] === 1 && col + dino.x < obstacleRight && col + dino.x > obstacleLeft && row + dino.y < obstacleBottom && row + dino.y > obstacleTop) {
+            return true;
+        }
     }
 
     return false;
 }
 
-// Función para mover al dinosaurio hacia arriba
+// Función que hace que el dinosaurio salte
 function moveUp() {
-    dino.y -= dinoSpeed;
+    // Solo permite saltar si el dinosaurio no está saltando actualmente
+    if (!dino.jumping) {
+        dino.jumping = true;
+        dino.speed = -10; // Establecer la velocidad inicial del salto
+    }
 }
 
 // Función para manejar los eventos de teclado
@@ -117,6 +160,3 @@ function handleKeyDown(event) {
 
 // Agregar un event listener a la ventana para manejar los eventos de teclado
 window.addEventListener("keydown", handleKeyDown);
-
-// Iniciar el bucle de juego
-gameLoop();
